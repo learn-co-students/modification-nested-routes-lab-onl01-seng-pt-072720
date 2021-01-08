93bb40b1d08863c331da84921e37dc38e3e38eeb
github username <github email address>
2021-01-08 15:17:06 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :artists do
+    '/artists/:artist_id'
+    
     resources :songs, only: [:index, :show, :new, :edit]
+
   end
   resources :songs
 end
